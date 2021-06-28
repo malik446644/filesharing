77d7bc0770bf993ae5257781d95fae591963f4f9
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-06-28 01:37:08 +0100

    made some progress

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 const electron = require("electron");
-const url = require("url");
-const path =  require("path");
 
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
-// listen for the app to be ready
 app.on("ready", function(){
     mainWindow = new BrowserWindow({});
-    // load html file 
-    // mainWindow.loadURL(`file://${path.join(__dirname, "index.html")}`);
-    mainWindow.loadURL(`https://facebook.com`);
+    mainWindow.loadFile("frontEnd/index.html");
 
-    // build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    // insert menu
     Menu.setApplicationMenu(mainMenu)
 })
 
@@ -25,3 +18,21 @@ const mainMenuTemplate = [
         label: 'file'
     }
 ]
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+      label: 'Developer Tools',
+      submenu:[
+        {
+          role: 'reload'
+        },
+        {
+          label: 'Toggle DevTools',
+          accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+          click(item, focusedWindow){
+            focusedWindow.toggleDevTools();
+          }
+        }
+      ]
+    });
+  }
