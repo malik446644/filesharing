e6bc5906ca92b8c3b33b51cca337b21b155ba573
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-06-30 02:59:58 +0100

    testing the ipc renderer

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,1 +1,9 @@
-console.log("woooow")
+const electron = require('electron');
+const {ipcRenderer} = electron;
+
+let device = document.querySelector(".device");
+
+device.addEventListener("click", () => {
+    console.log("this is working man")
+    ipcRenderer.send('item:add', item);
+})
