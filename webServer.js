e3def938bc192f8988864de57c92f289db0d81c4
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-07-01 18:08:03 +0100

    finished with recieving files through http and save in the machine storage

--- webServer.js
+++ webServer.js
@@ -1,13 +1,24 @@
 var http = require('http');
+var formidable = require('formidable');
+let fs = require("fs");
 
 //create a server object:
 module.exports = http.createServer(function (req, res) {
-    console.log("recieved a request")
-    req.on("data", (data) => {
-        console.log(data);
-    })
-    res.write('Hello World!');
-    res.end();
+    if (req.url == '/uploadFile') {
+        console.log("this is working fine man");
+        var form = new formidable.IncomingForm({multiples: true});
+        form.parse(req, function (err, fields, files) {
+            files.fileToUpload.forEach((file) => {
+                var oldpath = file.path;
+                var newpath = __dirname + "\\storage\\" + file.name;
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                    // res.write('File uploaded and moved!');
+                    // res.end();
+                });
+            })
+        });
+    }
 }).listen(8080);
 
 console.log("webServer is listening on port 8080")
