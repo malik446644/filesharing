6c31f721628fe4c849cc87f5c478081a14b8932f
MalikNajjar <59079190+malik446644@users.noreply.github.com>
2021-07-02 14:49:15 +0100

    made some progress

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -8,6 +8,11 @@ let currentDevicePublicAddress = document.querySelector(".currentDevicePublicAdd
 // selecting local devices list container
 let devicesContainer = document.querySelectorAll(".container")[1];
 
+// initilizing these DOM elements to use it gloabally
+let forms;
+let inputs;
+let sendImages;
+
 // send a signal to the electron app to give us (front end) the nesseccary data
 // this is sending the data only when the front end is able to read data
 ipcRenderer.send("giveMeData", null);
@@ -25,8 +30,27 @@ ipcRenderer.on("neccessaryData", (e, data) => {
             <form style="display: none;" class="theForm" action="http://${device.ip}:8080/uploadFile" method="post" enctype="multipart/form-data">
                 <input type="file" name="fileToUpload" class="fileToUpload" multiple>
             </form>
-            <img style="cursor: pointer;" src="https://cdn.iconscout.com/icon/free/png-512/send-forward-arrow-right-direction-import-30559.png" height="35" width="35"></img>
+            <img class="sendButton" style="cursor: pointer;" src="https://cdn.iconscout.com/icon/free/png-512/send-forward-arrow-right-direction-import-30559.png" height="35" width="35"></img>
         </div>`;
     })
     devicesContainer.innerHTML = devicesHTML;
-})
+
+    // selecting all the forms and the send images
+    forms = document.querySelectorAll(".theForm")
+    inputs = document.querySelectorAll(".fileToUpload")
+    sendImages = document.querySelectorAll(".sendButton")
+
+    // adding eventListeners for the send images
+    sendImages.forEach((sendImage, i) => {
+        sendImage.addEventListener("click", () => {
+            inputs[i].click();
+        });
+    });
+
+    // adding eventListeners for the inputs
+    inputs.forEach((input, i) => {
+        input.addEventListener("change", () => {
+            forms[i].submit();
+        });
+    });
+});
